d000b3c93a223879b99dd6bf409a7eaa4c6e4a14
Maciej Kucharczyk <maciej.kucharczyk@hotmail.com>
2024-12-14 15:26:47 +0100

    Install jextract with SDKMAN

--- prepare-jextract.sh
+++ prepare-jextract.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-function get_jextract() {
-  file=openjdk-22-jextract+5-33_linux-x64_bin.tar.gz
-  jextract_dir=jextract
-  if [ -d "$jextract_dir" ] ; then
-    echo "$jextract_dir already exist, skipping"
-    exit 0
-  fi
-  curl -O https://download.java.net/java/early_access/jextract/22/5/"$file"
-  tar -xf "$file"
-  rm -fv "$file"
-  mv -v jextract-22 "$jextract_dir"
-}
-
-get_jextract
+sdk install jextract 22.ea.6
 
 echo "Done"
